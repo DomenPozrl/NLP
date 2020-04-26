a6a08ccf331ccf65748607a6ce59bf2cab2cb301
DomenPozrl <40770999+DomenPozrl@users.noreply.github.com>
2020-04-26 18:18:14 +0200

    cleaning up the repo

---
+++ Attribute retrieval/order_data_for_learning.py
@@ -0,0 +1,72 @@
+import pickle
+
+def get_dict_from_vector(vector):
+
+	slovar = dict()
+	for i in range(len(vector)):
+		slovar[i] = vector[i]
+
+	return slovar
+
+if __name__ =="__main__":
+
+	znacilke_filename = "/home/stickman/School/NLP/Defense 2 urejen/znacilke_with_neigh.pickle"
+
+	data = pickle.load(open(znacilke_filename, "rb"))
+
+
+
+	vector_sentences = []
+	vector_vector_words = []
+	vector_vector_znacilke = []
+	vector_vector_classes = []
+
+
+	for sentence in data:
+
+		vector_sentences.append(sentence)
+		
+		words = []
+		znacilke = []
+		classes = []
+		
+		for p in data[sentence]:
+
+			head, vector = p
+
+			words.append([head[0], head[1], head[2]])
+			znacilke.append(get_dict_from_vector(vector))
+
+			if len(head) == 4:
+				classes.append(head[3])
+			else:
+				classes.append("notpropn")
+
+		vector_vector_words.append(words)
+		vector_vector_classes.append(classes)
+		vector_vector_znacilke.append(znacilke)
+
+
+
+	"""a = vector_vector_classes
+
+	for x in a:
+		print(x)
+		print("-----------------------------------------")"""
+
+
+	file = open("vector_sentences.pickle", "wb")
+	pickle.dump(vector_sentences, file)
+	file.close()
+
+	file = open("vector_vector_words.pickle", "wb")
+	pickle.dump(vector_vector_words, file)
+	file.close()	
+
+	file = open("vector_vector_znacilke.pickle", "wb")
+	pickle.dump(vector_vector_znacilke, file)
+	file.close()
+
+	file = open("vector_vector_classes.pickle", "wb")
+	pickle.dump(vector_vector_classes, file)
+	file.close()	
---
+++ parse_data.py
@@ -0,0 +1,141 @@
+
+import pickle
+
+def only_short_name_entity(p):
+	for i in range(len(p)):
+		element = p[i]
+
+		if "<name type=" in element:
+			if "</name>" in p[i + 2]:
+				continue
+			else:
+				return False
+
+	return True
+
+
+def has_propn(p):
+	for x in p:
+		if "<name type" in x:
+			return True
+	return False
+
+
+def process_sentence(p):	
+	
+	semafor = False
+	propn_buffer = ""
+
+	solution = []
+
+	for i in range(len(p)):
+
+		line = p[i]
+		
+
+		#get rid of useless meta lines however don't ignore the <name line because we need it's data
+		if line  and ("name" in line or line[0] != "<"):
+			
+			if "<name type" in line:
+				semafor = True
+				
+				#store they type of name into the buffer to be used in the next line
+				propn_buffer = line
+				continue
+
+			if "</name" in line:
+				semafor = False
+				continue
+
+			split_line = line.split()
+			
+			if semafor:
+
+				#this means this is a propn and the class is stored in propn_buffer
+				solution.append((split_line[0], split_line[1], split_line[2], propn_buffer.replace("<name type=", "").replace(">", "").replace('"', "")))
+			else:
+				#here we do all the work for normal lines
+				solution.append((split_line[0], split_line[1], split_line[2]))
+
+	return solution
+
+def construct_whole_sentence(solution):
+	s = ""
+	
+	for i in range(len(solution) -1):
+		element = solution[i]
+
+		if not solution[i+ 1][0].isalnum():
+			s += element[0]
+		else:
+			s+= element[0] + " "
+
+	s += solution[-1][0]
+	return s
+
+if __name__ == "__main__":
+	file = open("/home/stickman/School/NLP/Defense 2 urejen/Data/tagget_named_entities.vert", "r")
+
+	file_string = file.read()
+	file_string_split = file_string.split("</s>")
+	
+
+
+	
+	count_all = 0
+	count_short = 0
+	count_propn = 0
+
+	final_dict = dict()
+
+	for sentence in file_string_split:
+		
+		sentence_lines = sentence.split("\n")
+
+		if len(sentence_lines) > 1 and only_short_name_entity(sentence_lines):
+			a = process_sentence(sentence_lines)
+			s = construct_whole_sentence(a)
+
+			final_dict[s] = a
+			
+
+			print(s)
+			print("-------------------")
+			for key in a:
+				print(key)
+			print("-------------------")
+			
+
+		
+
+
+
+
+
+
+
+
+
+
+		#some simple statistics
+		if only_short_name_entity(sentence_lines):
+			count_short += 1
+		count_all += 1
+		
+		if has_propn(sentence_lines):
+			count_propn += 1
+
+
+
+	print(count_all)
+	print(count_short)
+	print(count_propn)
+		
+		
+
+
+
+
+	file = open("parsed_tagged_named_enteties.pickle", "wb")
+	pickle.dump(final_dict, file)
+	file.close()
