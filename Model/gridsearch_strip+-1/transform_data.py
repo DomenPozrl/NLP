cf05f5d3f4fcf85d9d8c46128fb93e4ae1b17a4a
Jan Jenko <jan.jenko94@gmail.com>
2020-05-09 21:04:29 +0200

    ru nexperiments with 1 or 0 neighbourhood only

---
+++ Model/gridsearch_strip+-1/transform_data.py
@@ -0,0 +1,21 @@
+import pickle
+
+with open('../../Data/strip+-2/vector_vector_znacilke.pickle', 'rb') as f:
+    data = pickle.load(f)
+
+data_modified = []
+for sentence in data:
+    sentence_modified = []
+    for word in sentence:
+        word_modified = {}
+        for key in word:
+            if key[0] != '1':
+                word_modified[key] = word[key]
+
+        sentence_modified.append(word_modified)
+
+    data_modified.append(sentence_modified)
+
+
+with open('../../Data/strip+-1/vector_vector_znacilke.pickle', 'wb') as f:
+    data = pickle.dump(data_modified, f)
