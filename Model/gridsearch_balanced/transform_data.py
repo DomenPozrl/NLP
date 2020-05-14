f24f2e24cc087753f43bed82fe3f270120f4843b
Jan Jenko <jan.jenko94@gmail.com>
2020-05-14 20:03:08 +0200

    balanced model

---
+++ Model/gridsearch_balanced/transform_data.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copy('../../Data/strip+-1/vector_vector_classes.pickle', '../../Data/balanced/vector_vector_classes.pickle')
+shutil.copy('../../Data/strip+-1/vector_vector_znacilke.pickle', '../../Data/balanced/vector_vector_znacilke.pickle')
